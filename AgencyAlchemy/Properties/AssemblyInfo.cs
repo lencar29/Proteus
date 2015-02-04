97bea13f3837784774acc649d242b06bfad9c129
lencar29 <lencar29@msn.com>
2015-02-03 23:05:22 -0600

    Added resource message

--- AgencyAlchemy/Properties/AssemblyInfo.cs
+++ AgencyAlchemy/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AgencyAlchemy")]
+[assembly: AssemblyTitle("Agency Alchemy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgencyAlchemy")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Maelstrom Solutions")]
+[assembly: AssemblyProduct("Agency Alchemy")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
